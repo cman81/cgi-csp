ce26b82a298b5b6abe8f998724858ff1af7b1f46
Christian <christian@christian-VirtualBox.(none)>
2013-10-24 10:32:24 -0400

    added support for detail container

--- js/global.js
+++ js/global.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.tile').each(function() {
+	$('.tile').each(function() { // TODO: remove this functionality, each tile should have a fixed color
 		var hue = 'rgb('
 			+ (Math.floor(Math.random() * 128)) + ','
 			+ (Math.floor(Math.random() * 128)) + ','
@@ -13,5 +13,21 @@ $(document).ready(function() {
 	});
 	$('.tile').mouseleave(function() {
 		$(this).removeClass('selected');
+	});
+	$('.tile').click(function() {
+		// reset
+		$('.tile').removeClass('active');
+		$('.detail-container').removeClass('active');
+				
+		$(this).addClass('active');
+		
+		// loop through classnames until you find one that begins with 'node-'
+		var classList = $(this).attr('class').split(/\s+/);
+		for (var i = 0; i < classList.length; i++) {
+			var item = classList[i];
+		    if (item.substring(0, 5) == 'node-') {
+		       $('.' + item).addClass('active');
+		    }
+		};
 	});
 });
